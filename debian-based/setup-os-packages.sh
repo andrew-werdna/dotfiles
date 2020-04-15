d9aa0b4c54b48cab4379fc66106d4cf562af5061
altair <andrew-werdna@users.noreply.github.com>
2020-04-15 09:33:17 -0500

    simplify setup, separate extra packages, delete old file

--- debian-based/setup-os-packages.sh
+++ debian-based/setup-os-packages.sh
@@ -1,18 +1,12 @@
 #!/usr/bin/env bash
 
-sudo apt install -y curl git build-essential neofetch feh scrot wget python-software-properties software-properties-common apt-transport-https lsb-release ca-certificates rsync vim emacs tmux zip unzip
+sudo apt install -y curl git build-essential neofetch feh scrot wget rsync vim emacs tmux zip unzip i3 rofi ranger compton
 
-# sudo apt install -y i3 awesome openbox obconf
+# other extras that may be needed at some point
+# sudo apt install -y python-software-properties software-properties-common apt-transport-https lsb-release ca-certificates
 
 # config git
 ghUser="andrew-werdna"
 git config --global user.email "$ghUser@users.noreply.github.com"
 git config --global user.name `whoami`
-
-# install my current fav desktop for ubuntu-based regolith [desktop](https://regolith-linux.org/docs/getting-started/install/)
-sudo add-apt-repository ppa:regolith-linux/release
-
-sudo apt update -y
-
-sudo apt install -y regolith-desktop
 
--- debian-based/update-bashrc.sh
+++ debian-based/update-bashrc.sh
@@ -1,27 +1,28 @@
 #!/usr/bin/env bash
 
 configFileName=".extrarc"
-filePath="$HOME/easy-new-machine-setup/debian-based/$configFileName"
+filePath="$HOME/dotfiles/debian-based/$configFileName"
+newFile=".$(whoami)rc"
+cp "$filePath" "$HOME/$newFile"
 
 read -r -d '' sourceCMD <<'EOF'
 
-path="$HOME/easy-new-machine-setup/debian-based/.extrarc"
+path="$HOME/.$(whoami)rc"
 if [ -f "$path" ]; then
-	echo -e "\n$path found, sourcing it now..."
 	source "$path"
 else
 	echo -e "\n$path was NOT found...\n"
 fi
 EOF
 
-searchCmd=`grep -i "$configFileName" ~/.bashrc`
+searchCmd=`grep -i "$newFile" ~/.bashrc`
 result=$?
 
 if [ "$result" -ne 0 ]; then
 	echo "$sourceCMD" >> ~/.bashrc
-	echo -e "\nscript with check has been added\n"
+	echo -e "\nupdating .bashrc with new source\n"
 else
-	echo -e "\nadding bash script to .bashrc is already done"
+	echo -e "\nupdating .bashrc is already done"
 fi
 
 exit 0
